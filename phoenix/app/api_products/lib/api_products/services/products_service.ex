0737aa577652d732bf257567452314e830f5cfc0
Caio Machado de Souza Gome <caiomachadodesouza1@gmail.com>
2022-01-17 15:30:16 -0300

    add data validation for create
    Checks have been added for cases where the information for creating a product is inadequate, so the sku field now only accepts alpha-numeric and hyphen values, the name field cannot be empty, the price can only be greater than 0 and the bar_code field must contain 8 to 10 digits.

--- phoenix/app/api_products/lib/api_products/services/products_service.ex
+++ phoenix/app/api_products/lib/api_products/services/products_service.ex
@@ -12,7 +12,8 @@ defmodule ApiProducts.ProductsService do
   end
 
   def create(product_params) do
-    with {:ok, product} <- Management.create_product(product_params) do
+    with {:ok, true} <- validate_parameters(product_params),
+         {:ok, product} <- Management.create_product(product_params) do
       RedisService.set_product(product)
       ElasticService.add_product(product)
       {:ok, product}
@@ -40,6 +41,38 @@ defmodule ApiProducts.ProductsService do
       RedisService.delete_product(product.id)
       ElasticService.delete_product(product.id)
       {:ok, result}
+    end
+  end
+
+  defp validate_parameters(params) do
+    confirm_param({:ok, true}, "sku", params["sku"])
+    |> confirm_param("price", params["price"])
+    |> confirm_param("bar_code", params["bar_code"])
+  end
+
+  defp confirm_param({:error, :bad_request, menssage}, _, _), do: {:error, :bad_request, menssage}
+
+  defp confirm_param({:ok, _true}, "sku", value) do
+    if String.match?(value, ~r/^([a-zA-Z0-9]|-)+$/) do
+      {:ok, true}
+    else
+      {:error, :bad_request, "sku can be only alphanumeric"}
+    end
+  end
+
+  defp confirm_param({:ok, _true}, "price", value) do
+    if value > 0 do
+      {:ok, true}
+    else
+      {:error, :bad_request, "the price must be greater than 0"}
+    end
+  end
+
+  defp confirm_param({:ok, _true}, "bar_code", value) do
+    if String.length(value) >= 8 && String.length(value) <= 10 do
+      {:ok, true}
+    else
+      {:error, :bad_request, "bar_code must be 8-10 digits"}
     end
   end
 end
