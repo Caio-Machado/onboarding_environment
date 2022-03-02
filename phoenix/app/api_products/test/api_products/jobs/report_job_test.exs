2b2ce5ac97e779ca8b100f1f025b4628bad3fde8
Caio Machado de Souza Gome <caiomachadodesouza1@gmail.com>
2022-03-02 13:59:55 -0300

    Update report_job_test
    Update the test perform/1with a product stored, now the product is created in the setup and not in the test anymore.
    
    Added a new test for perform/1, the test uses mock to test if there are no products registered in the database.

--- phoenix/app/api_products/test/api_products/jobs/report_job_test.exs
+++ phoenix/app/api_products/test/api_products/jobs/report_job_test.exs
@@ -1,35 +1,49 @@
 defmodule ApiProducts.ReportJobTest do
   use ApiProducts.DataCase
 
+  import Mock
+
   alias ApiProducts.ReportJob
   alias ApiProducts.Management
   alias ApiProducts.ReportService
   alias ApiProducts.ProductsService
 
   setup_all do
-    new_product = %{
+    expected_report =
+      "amount,barcode,description,id,name,price,sku\r\n50,123456789,Description,6216692d77161b03291e1f4c,Name,10.0,Sku\r\n"
+
+    [expected_report: expected_report]
+  end
+
+  setup do
+    Management.create_product(%{
+      "id" => "6216692d77161b03291e1f4c",
       "sku" => "Sku",
       "name" => "Name",
       "description" => "Description",
       "amount" => 50,
       "price" => 10,
       "barcode" => "123456789"
-    }
+    })
 
-    [new_product: new_product]
+    :ok
   end
 
   describe "perform/1" do
-    test "Write a product to the file", %{new_product: new_product} do
-      Management.create_product(new_product)
-      {:ok, products} = ProductsService.list(%{})
-      ReportJob.perform(%{"report" => "report"})
+    test "with a product stored", %{expected_report: expected_report} do
+      ReportJob.perform(%{"type" => "products"})
       {:ok, content} = File.read(ReportService.get_path())
-      Management.delete_all()
 
-      result = Enum.join(ReportService.generate_csv(products))
+      assert expected_report == content
+    end
 
-      assert result == content
+    test "with no product stored" do
+      with_mock(ProductsService, [], list: fn %{} -> {:ok, []} end) do
+        ReportJob.perform(%{"type" => "products"})
+        {:ok, content} = File.read(ReportService.get_path())
+
+        assert "" == content
+      end
     end
   end
 end
