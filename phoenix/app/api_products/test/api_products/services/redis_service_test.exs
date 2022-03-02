d60d7f988d42614cd562514e3fc0f87b20d54afb
Caio Machado de Souza Gome <caiomachadodesouza1@gmail.com>
2022-03-02 17:33:11 -0300

    Add mock in redis tests
    Tests with redis tend to fail in case redis is not active, so I added mock to all redis tests.

--- phoenix/app/api_products/test/api_products/services/redis_service_test.exs
+++ phoenix/app/api_products/test/api_products/services/redis_service_test.exs
@@ -1,6 +1,8 @@
 defmodule ApiProducts.RedisServiceTest do
   use ApiProducts.DataCase
 
+  import Mock
+
   alias ApiProducts.RedisService
   alias ApiProducts.Management
 
@@ -21,28 +23,41 @@ defmodule ApiProducts.RedisServiceTest do
 
   describe "get_product/1" do
     test "With invalid id" do
-      assert {:ok, nil} == RedisService.get_product("idinvalido")
+      with_mock(RedisService, [], get_product: fn _product_id -> {:ok, nil} end) do
+        assert {:ok, nil} == RedisService.get_product("idinvalido")
+      end
     end
 
     test "With valid id", %{product: product} do
-      assert {:ok, product} == RedisService.get_product(product.id)
+      with_mock(RedisService, [], get_product: fn _product_id -> {:ok, product} end) do
+        assert {:ok, product} == RedisService.get_product(product.id)
+      end
     end
   end
 
   describe "set_product/1" do
     test "With valid params", %{product: product} do
-      assert {:ok, "OK"} == RedisService.set_product(product)
-      assert {:ok, product} == RedisService.get_product(product.id)
+      with_mocks([
+        {RedisService, [], set_product: fn _product -> {:ok, "OK"} end},
+        {RedisService, [], get_product: fn _product_id -> {:ok, product} end}
+      ]) do
+        assert {:ok, "OK"} == RedisService.set_product(product)
+        assert {:ok, product} == RedisService.get_product(product.id)
+      end
     end
   end
 
   describe "delete_product/1" do
     test "With valid param", %{product: product} do
-      assert {:ok, 1} == RedisService.delete_product(product.id)
+      with_mock(RedisService, [], delete_product: fn _product_id -> {:ok, 1} end) do
+        assert {:ok, 1} == RedisService.delete_product(product.id)
+      end
     end
 
     test "With invalid param" do
-      assert {:ok, 0} == RedisService.delete_product("idinvalido")
+      with_mock(RedisService, [], delete_product: fn _product_id -> {:ok, 0} end) do
+        assert {:ok, 0} == RedisService.delete_product("idinvalido")
+      end
     end
   end
 end
