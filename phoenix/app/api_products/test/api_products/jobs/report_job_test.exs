076e1a787917db9de7f1814648be439327c29dba
Caio Machado de Souza Gome <caiomachadodesouza1@gmail.com>
2022-03-15 14:44:54 -0300

    Update report_job_test
    Now the report_job tests check if the route that sends the mail was called.

--- phoenix/app/api_products/test/api_products/jobs/report_job_test.exs
+++ phoenix/app/api_products/test/api_products/jobs/report_job_test.exs
@@ -1,5 +1,6 @@
 defmodule ApiProducts.ReportJobTest do
   use ApiProducts.DataCase
+  use HTTPoison.Base
 
   import Mock
 
@@ -11,8 +12,9 @@ defmodule ApiProducts.ReportJobTest do
 
   setup_all do
     expected_report = Fixtures.get_fixture(:expected_report)
+    mailer_url = Application.get_env(:api_products, :mailer_url)
 
-    [expected_report: expected_report]
+    [expected_report: expected_report, mailer_url: mailer_url]
   end
 
   setup do
@@ -30,18 +32,25 @@ defmodule ApiProducts.ReportJobTest do
   end
 
   describe "perform/1" do
-    test "with a product stored", %{expected_report: expected_report} do
-      ReportJob.perform(%{"type" => "products"})
-      {:ok, content} = File.read(ReportService.get_path())
-
-      assert expected_report == content
-    end
-
-    test "with no product stored" do
-      with_mock(ProductsService, [], list: fn %{} -> {:ok, []} end) do
+    test "with a product stored", %{expected_report: expected_report, mailer_url: mailer_url} do
+      with_mock(HTTPoison, [], post: fn _mailer_url, "" -> {:ok, %HTTPoison.Response{}} end) do
         ReportJob.perform(%{"type" => "products"})
         {:ok, content} = File.read(ReportService.get_path())
 
+        assert_called(HTTPoison.post(mailer_url, ""))
+        assert expected_report == content
+      end
+    end
+
+    test "with no product stored", %{mailer_url: mailer_url} do
+      with_mocks([
+        {ProductsService, [], list: fn %{} -> {:ok, []} end},
+        {HTTPoison, [], post: fn _mailer_url, "" -> {:ok, %HTTPoison.Response{}} end}
+      ]) do
+        ReportJob.perform(%{"type" => "products"})
+        {:ok, content} = File.read(ReportService.get_path())
+
+        assert_called(HTTPoison.post(mailer_url, ""))
         assert "" == content
       end
     end
